967cad0dc408a574d55a7d25901b606c0cad6a4e
Zoey Child <imperialoctopus@gmail.com>
2019-11-29 23:24:13 +0000

    Add appendix screen

---
+++ lib/screens/appendix/health_safety_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HealthSafetyScreen extends StatelessWidget {
+  const HealthSafetyScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Health and Safety'),
+      ),
+      body: Center(
+        child: Text('Greetings, Guardian.'),
+      ),
+    );
+  }
+}
