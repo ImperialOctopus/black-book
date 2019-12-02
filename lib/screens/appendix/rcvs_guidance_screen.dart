5341cbdffd46685a95a7fb9eda3c9c76d57117cc
Zoey Child <imperialoctopus@gmail.com>
2019-12-02 14:37:56 +0000

    Add RCVS guidance content

--- lib/screens/appendix/rcvs_guidance_screen.dart
+++ lib/screens/appendix/rcvs_guidance_screen.dart
@@ -9,8 +9,154 @@ class RcvsGuidanceScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('RCVS EMS Guidance'),
       ),
-      body: Center(
-        child: Text('Greetings, Guardian.'),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'Under current Regulations (Veterinary Surgeons Act 1966 and the Amendments of 1981 and 1993), you are entitled, as a veterinary student embarking upon your clinical studies, to perform the following:',
+            ),
+            Text(
+              '(a) examine animals;',
+            ),
+            RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.body1,
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '(b) carry out tests upon animals under the ',
+                  ),
+                  TextSpan(
+                    text: 'direction',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text: ' of a registered veterinary surgeon;',
+                  ),
+                ],
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.body1,
+                children: <TextSpan>[
+                  TextSpan(
+                    text:
+                        '(c) administer treatment (other than by way of surgical operations) to animals under the ',
+                  ),
+                  TextSpan(
+                    text: 'supervision',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text: ' of a registered veterinary surgeon; and',
+                  ),
+                ],
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.body1,
+                children: <TextSpan>[
+                  TextSpan(
+                    text:
+                        '(d) perform surgical operations upon animals in accordance with the directions and under the',
+                  ),
+                  TextSpan(
+                    text: 'direct and continuous personal supervision',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text: ' of a registered veterinary surgeon',
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8),
+            ),
+            RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.body1,
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'Note the ',
+                  ),
+                  TextSpan(
+                    text: 'bold',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text:
+                        ' terms. These indicate levels of control over your practical training which are exerted by your extra-mural teacher, and you should be aware of their meaning. The Council of the Royal College offers the following interpretation:',
+                  ),
+                ],
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.body1,
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '• ',
+                  ),
+                  TextSpan(
+                    text: 'Direction',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text:
+                        ': The veterinary surgeon gives the student directions in regard to the tests to be carried out but is not necessarily present when they are performed;',
+                  ),
+                ],
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.body1,
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '• ',
+                  ),
+                  TextSpan(
+                    text: 'Supervision',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text:
+                        ': The veterinary surgeon is present and in a position to respond to a request for assistance, but is not necessarily standing by the student’s side or even in the same room;',
+                  ),
+                ],
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.body1,
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '• ',
+                  ),
+                  TextSpan(
+                    text: 'Direct and continuous personal supervision',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text:
+                        ': The veterinary surgeon is present and giving the student (and the animal) his undivided attention.',
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              'Students must always ensure that they are acting under guidance',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
