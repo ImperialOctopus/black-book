e0881d602764b8772854bf712df4ddb50bcb19cd
Zoey Child <imperialoctopus@gmail.com>
2019-12-03 14:10:01 +0000

    Add body weight screen

--- lib/screens/appendix/body_weight_screen.dart
+++ lib/screens/appendix/body_weight_screen.dart
@@ -1,16 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+import '../../data/body_weight_data.dart';
 
 class BodyWeightScreen extends StatelessWidget {
   const BodyWeightScreen();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Body Weight Ratios'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Body Weight Ratios'),
+          bottom: TabBar(
+            tabs: <Tab>[
+              Tab(
+                text: 'Dog',
+                icon: Icon(MdiIcons.dog),
+              ),
+              Tab(
+                text: 'Cat',
+                icon: Icon(MdiIcons.cat),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            CustomScrollView(
+              slivers: <Widget>[
+                SliverAppBar(
+                  automaticallyImplyLeading: false,
+                  pinned: true,
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text(
+                        'Body Mass (Kg)',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      Text(
+                        'Surface Area (M²)',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ),
+                SliverList(
+                  delegate: SliverChildListDelegate.fixed(
+                    ListTile.divideTiles(
+                      context: context,
+                      tiles: _buildList(bodyWeightDataDog),
+                    ).toList(),
+                  ),
+                )
+              ],
+            ),
+            CustomScrollView(
+              slivers: <Widget>[
+                SliverAppBar(
+                  automaticallyImplyLeading: false,
+                  pinned: true,
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text(
+                        'Body Mass (Kg)',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      Text(
+                        'Surface Area (M²)',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ),
+                SliverList(
+                  delegate: SliverChildListDelegate.fixed(
+                    ListTile.divideTiles(
+                      context: context,
+                      tiles: _buildList(bodyWeightDataCat),
+                    ).toList(),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
-      body: Center(
-        child: Text('Greetings, Guardian.'),
+    );
+  }
+
+  List<ListTile> _buildList(List<MassAreaMap> map) {
+    return map
+        .map((MassAreaMap entry) => _buildRow(entry.mass, entry.area))
+        .toList();
+  }
+
+  ListTile _buildRow(String first, String second) {
+    return ListTile(
+      dense: true,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Text(first),
+          Text(second),
+        ],
       ),
     );
   }
