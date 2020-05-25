40438bf4b933e69c6b33840a7b6ceb1f6553bfea
Zoey Child <imperialoctopus@gmail.com>
2020-05-25 17:29:57 +0100

    Add new menu images

--- lib/screens/menu/home_screen.dart
+++ lib/screens/menu/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:black_book/screens/menu/menu_item.dart';
 import 'package:flutter/material.dart';
 
 import '../../model/model.dart';
+import '../../routes/enter_exit_route.dart';
+import '../appendix/appendix_screen.dart';
 import '../checklist/checklist_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,6 +15,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Black Book'),
+      ),
       body: ConstrainedBox(
         constraints: BoxConstraints(maxWidth: 600),
         child: Column(
@@ -25,7 +30,25 @@ class HomeScreen extends StatelessWidget {
                 'assets/images/checklist.jpg',
                 fit: BoxFit.cover,
               ),
-              target: ChecklistScreen(model: model),
+              onTap: () => Navigator.of(context).push(
+                EnterExitRoute(
+                  enterPage: ChecklistScreen(model: model),
+                  exitPage: this,
+                ),
+              ),
+            ),
+            MenuItem(
+              name: 'Practical Sign-Off',
+              child: Image.asset(
+                'assets/images/practical.jpg',
+                fit: BoxFit.cover,
+              ),
+              onTap: () => Navigator.of(context).push(
+                EnterExitRoute(
+                  enterPage: ChecklistScreen(model: model),
+                  exitPage: this,
+                ),
+              ),
             ),
             MenuItem(
               name: 'Appendix',
@@ -33,50 +56,27 @@ class HomeScreen extends StatelessWidget {
                 'assets/images/books.jpg',
                 fit: BoxFit.cover,
               ),
-              target: AppendixScreen(),
+              onTap: () => Navigator.of(context).push(
+                EnterExitRoute(
+                  enterPage: AppendixScreen(),
+                  exitPage: this,
+                ),
+              ),
+            ),
+            MenuItem(
+              name: 'Export',
+              child: Image.asset(
+                'assets/images/export.jpg',
+                fit: BoxFit.cover,
+              ),
+              onTap: () => Navigator.of(context).push(
+                EnterExitRoute(
+                  enterPage: AppendixScreen(),
+                  exitPage: this,
+                ),
+              ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget menuItem(
-      {BuildContext context, String name, String imageAsset, Function onTap}) {
-    return ConstrainedBox(
-      constraints: BoxConstraints(maxHeight: 200),
-      child: Card(
-        color: Theme.of(context).primaryColor,
-        semanticContainer: true,
-        clipBehavior: Clip.antiAlias,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        elevation: 5,
-        child: InkWell(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Expanded(
-                child: Image.asset(
-                  imageAsset,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: EdgeInsets.only(right: 4),
-                  child: Text(
-                    name,
-                    textAlign: TextAlign.end,
-                    style: Theme.of(context).primaryTextTheme.headline6,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          onTap: onTap,
         ),
       ),
     );
--- lib/screens/menu/menu_item.dart
+++ lib/screens/menu/menu_item.dart
@@ -6,40 +6,40 @@ class MenuItem extends StatelessWidget {
 
   final String name;
   final Widget child;
-  final Widget target;
+  final int flex;
+  final Function onTap;
 
-  const MenuItem({this.name, this.child, this.target});
+  const MenuItem({this.name, this.child, this.flex = 1, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: BoxConstraints(maxHeight: maxHeight),
-      child: InkWell(
-        child: Stack(
-          children: <Widget>[
-            Positioned.fill(child: child),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                color: Theme.of(context).primaryColor.withAlpha(labelAlpha),
-                child: Padding(
-                  padding: EdgeInsets.only(right: 4),
-                  child: Text(
-                    name,
-                    textAlign: TextAlign.end,
-                    style: Theme.of(context).primaryTextTheme.headline6,
+    return Expanded(
+      flex: flex,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxHeight: maxHeight),
+        child: InkWell(
+          child: Stack(
+            children: <Widget>[
+              Positioned.fill(child: child),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  color: Theme.of(context).primaryColor.withAlpha(labelAlpha),
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 4),
+                    child: Text(
+                      name,
+                      textAlign: TextAlign.end,
+                      style: Theme.of(context).primaryTextTheme.headline6,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-        onTap: () => Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (_) => target,
+            ],
           ),
+          onTap: onTap,
         ),
       ),
     );
