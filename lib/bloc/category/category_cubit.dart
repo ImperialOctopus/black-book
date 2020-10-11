04f47626676da811de27aa33fcf7c8e548a8c9c5
Zoey Child <imperialoctopus@gmail.com>
2020-10-11 13:57:30 +0100

    Add category cubit

--- lib/bloc/category/category_cubit.dart
+++ lib/bloc/category/category_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
 
 class CategoryCubit extends Cubit<int> {
-  CategoryCubit(int state) : super(state);
+  CategoryCubit() : super(0);
+
+  void setIndex(int index) => emit(index);
 }
