62ee73eb16488da2e7b44f47d096ed793280f4e4
Zoey Child <imperialoctopus@gmail.com>
2020-10-18 19:34:23 +0100

    Add signature screen button

--- lib/black_book_app.dart
+++ lib/black_book_app.dart
@@ -14,6 +14,7 @@ import 'screen/checklist/checklist_category_screen.dart';
 import 'screen/checklist/checklist_screen.dart';
 import 'screen/home/home_screen.dart';
 import 'screen/practical/practical_screen.dart';
+import 'screen/practical/signature_screen.dart';
 import 'service/model_service.dart';
 import 'theme/style.dart';
 
@@ -60,6 +61,7 @@ class _AppViewState extends State<AppView> {
         '/checklist': (var context) => ChecklistScreen(),
         '/checklist/category': (var context) => ChecklistCategoryScreen(),
         '/practical': (var context) => PracticalScreen(),
+        '/practical/signature': (var context) => SignatureScreen(),
         '/appendix': (var context) => AppendixScreen(),
         '/appendix/addresses': (var context) => AddressesScreen(),
         '/appendix/biodata': (var context) => BioDataScreen(),
--- lib/screen/practical/signature_screen.dart
+++ lib/screen/practical/signature_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SignatureScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text('Hello')),
+    );
   }
 }
