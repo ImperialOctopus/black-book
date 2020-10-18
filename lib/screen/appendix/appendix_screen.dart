31f860accbbf878a41a0d4a23a4756947373a3c8
Zoey Child <imperialoctopus@gmail.com>
2020-10-18 18:42:18 +0100

    Update to null safety

--- lib/screen/appendix/appendix_screen.dart
+++ lib/screen/appendix/appendix_screen.dart
@@ -15,49 +15,49 @@ class AppendixScreen extends StatelessWidget {
               title: const Text('Health & Safety'),
               trailing: Icon(Icons.keyboard_arrow_right),
               onTap: () {
-                Navigator.of(context).pushNamed('/appendix/safety');
+                Navigator.of(context)?.pushNamed('/appendix/safety');
               },
             ),
             ListTile(
               title: const Text('RCVS EMS Guidance'),
               trailing: Icon(Icons.keyboard_arrow_right),
               onTap: () {
-                Navigator.of(context).pushNamed('/appendix/rcvs');
+                Navigator.of(context)?.pushNamed('/appendix/rcvs');
               },
             ),
             ListTile(
               title: const Text('Biological Data'),
               trailing: Icon(Icons.keyboard_arrow_right),
               onTap: () {
-                Navigator.of(context).pushNamed('/appendix/biodata');
+                Navigator.of(context)?.pushNamed('/appendix/biodata');
               },
             ),
             ListTile(
               title: const Text('Body Weight Ratios'),
               trailing: Icon(Icons.keyboard_arrow_right),
               onTap: () {
-                Navigator.of(context).pushNamed('/appendix/weight');
+                Navigator.of(context)?.pushNamed('/appendix/weight');
               },
             ),
             ListTile(
               title: const Text('Laboratory Samples & Profiles'),
               trailing: Icon(Icons.keyboard_arrow_right),
               onTap: () {
-                Navigator.of(context).pushNamed('/appendix/labprofiles');
+                Navigator.of(context)?.pushNamed('/appendix/labprofiles');
               },
             ),
             ListTile(
               title: const Text('Normal Laboratory Values'),
               trailing: Icon(Icons.keyboard_arrow_right),
               onTap: () {
-                Navigator.of(context).pushNamed('/appendix/labnormal');
+                Navigator.of(context)?.pushNamed('/appendix/labnormal');
               },
             ),
             ListTile(
               title: const Text('Useful Addresses'),
               trailing: Icon(Icons.keyboard_arrow_right),
               onTap: () {
-                Navigator.of(context).pushNamed('/appendix/addresses');
+                Navigator.of(context)?.pushNamed('/appendix/addresses');
               },
             ),
           ],
