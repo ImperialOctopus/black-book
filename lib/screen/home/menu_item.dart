c2c972349def4673dd1b8401c119e90ac2d88b5e
Zoey Child <imperialoctopus@gmail.com>
2020-10-19 20:19:09 +0100

    Add maximum width to menu items

--- lib/screen/home/menu_item.dart
+++ lib/screen/home/menu_item.dart
@@ -1,46 +1,42 @@
 import 'package:flutter/material.dart';
 
 class MenuItem extends StatelessWidget {
-  final double maxHeight = 200;
   final int labelAlpha = 80;
 
   final String name;
   final Widget child;
-  final int flex;
+  final double maxHeight;
   final Function() onTap;
 
-  const MenuItem({this.name, this.child, this.flex = 1, this.onTap});
+  const MenuItem({this.name, this.child, this.maxHeight = 200, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: flex,
-      child: ConstrainedBox(
-        constraints: BoxConstraints(maxHeight: maxHeight),
-        child: InkWell(
-          child: Stack(
-            children: <Widget>[
-              Positioned.fill(child: child),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  color: Theme.of(context).primaryColor.withAlpha(labelAlpha),
-                  child: Padding(
-                    padding: EdgeInsets.only(right: 4),
-                    child: Text(
-                      name,
-                      textAlign: TextAlign.end,
-                      style: Theme.of(context).primaryTextTheme.headline6,
-                    ),
+    return ConstrainedBox(
+      constraints: BoxConstraints(maxHeight: maxHeight),
+      child: InkWell(
+        child: Stack(
+          children: <Widget>[
+            Positioned.fill(child: child),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Theme.of(context).primaryColor.withAlpha(labelAlpha),
+                child: Padding(
+                  padding: EdgeInsets.only(right: 4),
+                  child: Text(
+                    name,
+                    textAlign: TextAlign.end,
+                    style: Theme.of(context).primaryTextTheme.headline6,
                   ),
                 ),
               ),
-            ],
-          ),
-          onTap: onTap,
+            ),
+          ],
         ),
+        onTap: onTap,
       ),
     );
   }
