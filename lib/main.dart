d747b234238cfb9bcb4fd8af143e074ecead14a4
Zoey Child <imperialoctopus@gmail.com>
2020-10-24 14:33:33 +0100

    Add point adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'black_book_app.dart';
+import 'model/signature/point_type_adapter.dart';
+import 'model/signature/stroke.dart';
 import 'service/checklist_service.dart';
 import 'service/signature_service.dart';
 
 void main() async {
   await Hive.initFlutter();
+  Hive.registerAdapter<Stroke>(StrokeAdapter());
+  Hive.registerAdapter<Point>(PointAdapter());
 
   final checklistService = await ChecklistService.open('black-book');
   final signatureService = await SignatureService.open('black-book-signature');
