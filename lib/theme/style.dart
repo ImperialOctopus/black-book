9dfc20fb43078d742fa0cdb311d32735e65b8321
Zoey Child <imperialoctopus@gmail.com>
2020-10-25 20:25:58 +0000

    Update app style

--- lib/theme/style.dart
+++ lib/theme/style.dart
@@ -4,18 +4,7 @@ ThemeData themeData = ThemeData(
   // Define the default brightness and colors.
   brightness: Brightness.light,
   primaryColor: Colors.grey[800],
-  accentColor: Colors.purpleAccent[100],
-
-  // Define the default font family.
-  fontFamily: 'Montserrat',
-
-  // Define the default TextTheme. Use this to specify the default
-  // text styling for headlines, titles, bodies of text, and more.
-  textTheme: const TextTheme(
-    headline5: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-    headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-    bodyText2: TextStyle(fontSize: 16.0),
-  ),
+  accentColor: Colors.blue[100],
 
   dividerTheme: const DividerThemeData(
     thickness: 1,
