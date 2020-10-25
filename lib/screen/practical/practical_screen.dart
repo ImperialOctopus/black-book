7dc064a2f2a1663b8b905339c2fa1dd13fc350eb
Zoey Child <imperialoctopus@gmail.com>
2020-10-25 15:05:11 +0000

    Add practical screen card layout

--- lib/screen/practical/practical_screen.dart
+++ lib/screen/practical/practical_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 import '../../bloc/signature_select/signature_select_cubit.dart';
 
@@ -7,38 +8,43 @@ class PracticalScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Practical Sign-Off')),
-        body: Center(
-          child: FractionallySizedBox(
-            widthFactor: 0.6,
+      appBar: AppBar(title: Text('Practical Sign-Off')),
+      body: ListView(
+        children: [
+          Card(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                OutlinedButton(
-                    child: Text('Small Animal Clinical Studies'),
-                    onPressed: () {
-                      BlocProvider.of<SignatureSelectCubit>(context)
-                          .setReference('4_clinical_small');
-                      Navigator.of(context).pushNamed('/practical/signature');
-                    }),
-                OutlinedButton(
-                    child: Text('Farm Animal Clinical Studies'),
-                    onPressed: () {
-                      BlocProvider.of<SignatureSelectCubit>(context)
-                          .setReference('4_clinical_farm');
-                      Navigator.of(context).pushNamed('/practical/signature');
-                    }),
-                OutlinedButton(
-                    child: Text('Equine Clinical Studies'),
-                    onPressed: () {
-                      BlocProvider.of<SignatureSelectCubit>(context)
-                          .setReference('4_clinical_equine');
-                      Navigator.of(context).pushNamed('/practical/signature');
-                    }),
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(MdiIcons.rabbit),
+                  title: Text('Fourth Year - Small Animal Clinical Studies'),
+                ),
+                ListTile(
+                  title: Text('Session 1'),
+                  trailing: Container(
+                    height: 100,
+                    width: 200,
+                    decoration: BoxDecoration(
+                        border: Border(
+                            left: BorderSide(
+                                width: 2.0, color: Colors.lightBlue[100]))),
+                    child: FittedBox(
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  onTap: () => openSignaturePage(context, '4_clinical_small'),
+                ),
               ],
             ),
           ),
-        ));
+        ],
+      ),
+    );
+  }
+
+  void openSignaturePage(BuildContext context, String reference) {
+    BlocProvider.of<SignatureSelectCubit>(context)
+        .setReference('4_clinical_small');
+    Navigator.of(context).pushNamed('/practical/signature');
   }
 }
