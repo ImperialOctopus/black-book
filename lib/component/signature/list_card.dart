2b5f0687625d5ef290473f858ffaae2440fe485e
Zoey Child <imperialoctopus@gmail.com>
2020-10-25 18:29:14 +0000

    Add static signature container component

--- lib/component/signature/list_card.dart
+++ lib/component/signature/list_card.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ListCard extends StatelessWidget {
   final List<Widget> children;
 
-  ListCard({@required this.children});
+  const ListCard({@required this.children});
 
   @override
   Widget build(BuildContext context) {
